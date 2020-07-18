8004bb6e104c7c38c8ed55c8a45de7459fd54f68
Federico Fuga <fuga@studiofuga.com>
2020-07-18 11:11:37 +0200

    ShapeFileViewer: added map and layer

--- QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.cpp
+++ QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.cpp
@@ -6,10 +6,22 @@
 
 #include <QApplication>
 
+using namespace qmapcontrol;
+
 ShapeFilesViewer::ShapeFilesViewer()
 : QMainWindow(nullptr)
 {
+    map = new QMapControl(QSizeF(800,600),this);
 
+    baseAdapter = std::make_shared<MapAdapterOSM>();
+    baseMap = std::make_shared<LayerMapAdapter>("OpenStreetMap", baseAdapter);
+
+    map->addLayer(baseMap);
+
+    map->setMapFocusPoint(PointWorldCoord(  -77.042793, -12.046374));
+    map->setZoom(9);
+
+    setCentralWidget(map);
 }
 
 
@@ -18,6 +30,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     ShapeFilesViewer mainWindow;
+    mainWindow.resize(800, 600);
     mainWindow.show();
 
     return app.exec();

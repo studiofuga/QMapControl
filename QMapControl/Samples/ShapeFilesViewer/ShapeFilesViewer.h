f9b5259f97192fcd606c7d5a439452ac93dc8f48
Federico Fuga <fuga@studiofuga.com>
2020-07-18 11:11:37 +0200

    ShapeFileViewer: added map and layer

--- QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.h
+++ QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.h
@@ -5,11 +5,22 @@
 #ifndef QMAPCONTROL_SHAPEFILESVIEWER_H
 #define QMAPCONTROL_SHAPEFILESVIEWER_H
 
+#include "QMapControl/QMapControl.h"
+#include "QMapControl/MapAdapterOSM.h"
+#include "QMapControl/LayerMapAdapter.h"
+
 #include <QtWidgets/QMainWindow>
+
+#include <memory>
 
 class ShapeFilesViewer : public QMainWindow
 {
     Q_OBJECT
+
+    qmapcontrol::QMapControl *map;
+    std::shared_ptr<qmapcontrol::MapAdapterOSM> baseAdapter;
+    std::shared_ptr<qmapcontrol::LayerMapAdapter> baseMap;
+
 public:
     ShapeFilesViewer();
 };

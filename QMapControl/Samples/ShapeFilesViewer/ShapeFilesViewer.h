4da46057da08bdbb7885b8b52382fe89627ec604
Federico Fuga <fuga@studiofuga.com>
2020-07-18 10:55:01 +0200

    Skeleton Shapefile Viewer example added

---
+++ QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.h
@@ -0,0 +1,17 @@
+//
+// Created by fuga on 18/07/2020.
+//
+
+#ifndef QMAPCONTROL_SHAPEFILESVIEWER_H
+#define QMAPCONTROL_SHAPEFILESVIEWER_H
+
+#include <QtWidgets/QMainWindow>
+
+class ShapeFilesViewer : public QMainWindow
+{
+    Q_OBJECT
+public:
+    ShapeFilesViewer();
+};
+
+#endif // QMAPCONTROL_SHAPEFILESVIEWER_H

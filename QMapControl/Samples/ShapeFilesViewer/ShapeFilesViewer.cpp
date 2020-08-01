98cc996cf6fac3c6151829ab9677ab73b1bdd597
Federico Fuga <fuga@studiofuga.com>
2020-08-01 18:21:50 +0200

    Samples: added code to read raster (tiff) files
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.cpp
+++ QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.cpp
@@ -11,6 +11,7 @@
 #include <QMessageBox>
 
 #include <QDebug>
+#include <QSettings>
 
 #include <stdexcept>
 #include <sstream>
@@ -36,32 +37,37 @@ ShapeFilesViewer::ShapeFilesViewer()
     buildMenu();
 }
 
-void ShapeFilesViewer::buildMenu() {
+void ShapeFilesViewer::buildMenu()
+{
     auto fileMenu = menuBar()->addMenu(tr("&File"));
     auto actionSelectShapeFile = new QAction("Load Shapefile...");
     fileMenu->addAction(actionSelectShapeFile);
 
     connect(actionSelectShapeFile, &QAction::triggered, this, &ShapeFilesViewer::onLoadShapeFile);
+
+    auto actionSelectTiffFile = new QAction("Load Tiff...");
+    fileMenu->addAction(actionSelectTiffFile);
+
+    connect(actionSelectTiffFile, &QAction::triggered, this, &ShapeFilesViewer::onLoadTiffFile);
 }
 
-void ShapeFilesViewer::onLoadShapeFile() {
-    try
-    {
-        qDebug() << "Triggered";
-        auto shapefile
-            = QFileDialog::getOpenFileName(this, tr("Select Shapefile to load"), QString(),
-                                           tr("ShapeFiles (*.shp);;All files (*.*)"));
+void ShapeFilesViewer::onLoadShapeFile()
+{
+    QSettings settings;
 
-        qDebug() << "Selected: " << shapefile;
-        if(!shapefile.isEmpty())
-        {
-            if (shpDataSet != nullptr)
+    try {
+        auto basedir = settings.value("shapefiledir").toString();
+        auto shapefile = QFileDialog::getOpenFileName(this, tr("Select Shapefile to load"), basedir,
+                                                      tr("ShapeFiles (*.shp);;All files (*.*)"));
+
+        if (!shapefile.isEmpty()) {
+            if (shpDataSet != nullptr) {
                 delete shpDataSet;
+            }
 
-            shpDataSet = (GDALDataset*)OGROpen(shapefile.toStdString().c_str(), 0, nullptr);
-            if(!shpDataSet)
-            {
-                std::ostringstream  ss;
+            shpDataSet = (GDALDataset *) OGROpen(shapefile.toStdString().c_str(), 0, nullptr);
+            if (!shpDataSet) {
+                std::ostringstream ss;
                 ss << "Can't load shapefile " << shapefile.toStdString() << ": ";
                 throw std::runtime_error(ss.str());
             }
@@ -82,9 +88,56 @@ void ShapeFilesViewer::onLoadShapeFile() {
 
             map->addLayer(shpLayer);
             shpLayer->setVisible(true);
+
+            QFileInfo info(shapefile);
+            settings.setValue("shapefiledir", info.absolutePath());
         }
-    } catch (std::exception &x) {
+    }
+    catch (std::exception &x) {
         QMessageBox::warning(this, "Error loading shapefile", x.what());
+    }
+}
+
+void ShapeFilesViewer::onLoadTiffFile()
+{
+    QSettings settings;
+
+    try {
+        auto basedir = settings.value("tifffiledir").toString();
+        auto tiffFileName = QFileDialog::getOpenFileName(this, tr("Select Tiff to load"), basedir,
+                                                         tr("TIFF Files (*.tif);;All files (*.*)"));
+
+        if (!tiffFileName.isEmpty()) {
+            if (tiffDataSet != nullptr) {
+                delete tiffDataSet;
+            }
+
+            tiffDataSet = (GDALDataset *) GDALOpen(tiffFileName.toStdString().c_str(), GA_ReadOnly);
+
+            double adfGeoTransform[6];
+            qDebug() << "Driver: " << tiffDataSet->GetDriver()->GetDescription() << " - "
+                     << tiffDataSet->GetDriver()->GetMetadataItem(GDAL_DMD_LONGNAME);
+            qDebug() << "Size:" << tiffDataSet->GetRasterXSize() << tiffDataSet->GetRasterYSize()
+                     << tiffDataSet->GetRasterCount();
+            if (tiffDataSet->GetProjectionRef() != NULL)
+                qDebug() << "Projection: " << tiffDataSet->GetProjectionRef();
+            if (tiffDataSet->GetGeoTransform(adfGeoTransform) == CE_None) {
+                qDebug() << "Origin = (" << adfGeoTransform[0] << "," << adfGeoTransform[3] << ")";
+                qDebug() << "Pixel Size = (" << adfGeoTransform[1] << "," << adfGeoTransform[5];
+            }
+
+            tiffAdapter = std::make_shared<AdapterRaster>(tiffDataSet, "");
+            tiffLayer = std::make_shared<LayerRaster>("Tiff-Layer");
+            tiffLayer->addRaster(tiffAdapter);
+
+            map->addLayer(tiffLayer);
+            tiffLayer->setVisible(true);
+
+            QFileInfo info(tiffFileName);
+            settings.setValue("tifffiledir", info.absolutePath());
+        }
+    }
+    catch (std::exception &x) {
     }
 }
 
--- QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.h
+++ QMapControl/Samples/ShapeFilesViewer/ShapeFilesViewer.h
@@ -5,28 +5,34 @@
 #ifndef QMAPCONTROL_SHAPEFILESVIEWER_H
 #define QMAPCONTROL_SHAPEFILESVIEWER_H
 
-#include "QMapControl/QMapControl.h"
-#include "QMapControl/MapAdapterOSM.h"
-#include "QMapControl/LayerMapAdapter.h"
 #include "QMapControl/ESRIShapefile.h"
 #include "QMapControl/LayerESRIShapefile.h"
+#include "QMapControl/LayerMapAdapter.h"
+#include "QMapControl/MapAdapterOSM.h"
+#include "QMapControl/QMapControl.h"
+
+#include "QMapControl/AdapterRaster.h"
+#include "QMapControl/LayerRaster.h"
 
 #include <QtWidgets/QMainWindow>
 
 #include <memory>
 
-class ShapeFilesViewer : public QMainWindow
-{
-    Q_OBJECT
+class ShapeFilesViewer : public QMainWindow {
+Q_OBJECT
 
     qmapcontrol::QMapControl *map;
     std::shared_ptr<qmapcontrol::MapAdapterOSM> baseAdapter;
     std::shared_ptr<qmapcontrol::LayerMapAdapter> baseLayer;
 
+    GDALDataset *shpDataSet = nullptr;
     std::shared_ptr<qmapcontrol::ESRIShapefile> shpAdapter;
     std::shared_ptr<qmapcontrol::LayerESRIShapefile> shpLayer;
 
-    GDALDataset *shpDataSet = nullptr;
+    GDALDataset *tiffDataSet = nullptr;
+    std::shared_ptr<qmapcontrol::AdapterRaster> tiffAdapter;
+    std::shared_ptr<qmapcontrol::LayerRaster> tiffLayer;
+
 public:
     ShapeFilesViewer();
 
@@ -34,7 +40,10 @@ private:
     void buildMenu();
 
 public slots:
+
     void onLoadShapeFile();
+
+    void onLoadTiffFile();
 };
 
 #endif // QMAPCONTROL_SHAPEFILESVIEWER_H
